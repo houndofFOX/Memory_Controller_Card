b5a646d1e1f17a375f6f36f2c2a7c760f2d13c56
houndofFOX <samuel.akira@gmail.com>
2019-11-27 13:24:34 -0800

    Updated memory module and added consideration for some edge cases in the state machine.

--- src/memory_card_top.vhd
+++ src/memory_card_top.vhd
@@ -29,6 +29,8 @@ entity mem_card_top is
   );
 end mem_card_top;
 
+architecture mem_card_top of mem_card_top is
+  
 --------------------- CONSTANTS ---------------------
   -- PCI Commands
   constant INTERRUPT_ACK      : natural := 16#00#;
@@ -49,21 +51,25 @@ end mem_card_top;
 
 ---------------------- SIGNALS ----------------------
   signal s_addr             : std_logic_vector(31 downto 0);
-  
+  signal s_readnWrite       : std_logic;    -- active high
+  signal s_upper_data       : std_logic_vector(31 downto 0);
+  signal s_cs               : std_logic_vector(7 downto 0);  -- active high
+  signal s_datas            : t_array_slv (0 to 7)(7 downto 0);
+  signal s_data_shadow      : std_logic_vector(63 downto 0);
+  signal s_shadow_valid     : std_logic;    -- active high
+  --signal s_data_lower_curr  : std_logic_vector(31 downto 0);
+  --signal s_data_upper_curr  : std_logic_vector(31 downto 0);
+  --signal s_data_lower_next  : std_logic_vector(31 downto 0);
+  --signal s_data_upper_next  : std_logic_vector(31 downto 0);
 
-process (i_clk) is
-
-architecture mem_card_top of mem_card_top is
-  
   type t_state is
     (
       IDLE,
-      READ32_TA,
+      READ_TA,
       READ32_WAIT,
       READ32,
       WRITE32_WAIT,
       WRITE32,
-      READ64_TA,
       READ64_WAIT,
       READ64,
       WRITE64_WAIT,
@@ -74,8 +80,9 @@ architecture mem_card_top of mem_card_top is
 
 begin
   -- State Machine
-  stateMachine: process(i_reset, i_clk)
+  process(i_reset, i_clk) is
   begin
+    -- Check reset
     if (i_reset = '1') then
       io_addr_data <= (others => '0');
       io_data_upper <= (others => '0');
@@ -83,7 +90,12 @@ begin
       o_trdy_l <= '1';
       o_ack64_l <= '1';
       o_stop <= '1';
+      s_data_lower_curr <= (others => '0');
+      s_data_upper_curr <= (others => '0');
+      s_data_lower_next <= (others => '0');
+      s_data_upper_next <= (others => '0');
       s_state <= IDLE;
+    -- Normal Operation
     elsif(rising_edge(i_clk)) then
       case s_state is
         when IDLE =>
@@ -93,68 +105,109 @@ begin
           o_devsel_l    <= 'Z';
           o_trdy_l      <= 'Z';
           o_ack64_l     <= 'Z';
-          o_stop        <= 'Z';
-          -- 
+          o_stop_l      <= 'Z';
+          -- Set signal defaults
           s_addr      <= (others => '0');
-          -- Wait for Frame assert
-          if (i_frame_l = '0') then
-            -- Check address
-            if (io_addr_data(31 downto 18) = ZERO_ADDR and i_cbe_lower_l /= DUAL_ADDR_CYCLE) then
-              -- Assert device select active
-              o_devsel_l  <= '0';
-              -- Assert TRDY inactive
-              o_trdy_l    <= '1';
-              -- Save addr to local register
-              s_addr <= io_addr_data;
-              -- Memory Read
-              if (i_cbe_lower_l = MEM_READ) then
-                -- 64 bit read mode
-                if (i_req64_l = '0') then
-                  o_ack64_l <= '0';
-                  state <= READ64_TA;
-                -- 32 bit read mode
-                else
-                  o_ack64_l <= '1';
-                  state <= READ32_TA;
-                end if;
-              -- Memory Write
-              elsif (i_cbe_lower_l = MEM_WRITE) then
-                -- 64 bit write mode
-                if(i_req64_l = '0') then
-                  o_ack64_l <= '0';
-                  state <= WRITE64_WAIT;
-                -- 32 bit write mode
-                else
-                  o_ack64_l <= '1';
-                  state <= WRITE32_WAIT;
-                end if;
-              else
-                -- Issue stop, command not supported.
-                o_stop <= '0';
-                state <= STOP_TERM;
-              end if;
+          s_readnWrite <= '1';
+          s_cs = (others => '0');
+          s_data_shadow <= (others =>'0');
+          s_shadow_valid <= '0';
+          -- Wait for Frame assert, check address and dual address mode
+          if (i_frame_l = '0' and io_addr_data(31 downto 18) = ZERO_ADDR and i_cbe_lower_l /= DUAL_ADDR_CYCLE) then
+            -- o_stop_l default
+            o_stop_l <= '1';
+            -- Assert device select active
+            o_devsel_l  <= '0';
+            -- Assert TRDY inactive
+            o_trdy_l    <= '1';
+            -- Set ACK64 output
+            o_ack64_l <= '0' when i_req64_l = '0' else '1';
+            -- Configure initial chip select
+            if (i_req64_l = '0') then
+              s_cs = (others => '1');
+            elsif (io_addr_data(2) = '1') then
+              s_cs(7 downto 4) <= (others => '1');
+            else
+              s_cs(3 downto 0) <= (others => '1');
             end if;
+            -- Save addr to local register
+            s_addr <= io_addr_data;
+            -- Memory Read
+            if (i_cbe_lower_l = MEM_READ) then
+              -- Set read not write
+              s_readnWrite = '1';
+              -- Next state: Read Turnaround
+              state <= READ_TA;
+            -- Memory Write
+            elsif (i_cbe_lower_l = MEM_WRITE) then
+              -- Set not "read not write"
+              s_readnWrite <= '0';
+              -- Next State
+              state <= WRITE64_WAIT when i_req64_l = '0' else WRITE32_WAIT
+            -- Issue stop, command not supported.
+            else
+              o_stop <= '0';
+              state <= STOP_TERM;
+            end if;
+            
           end if;
-        when READ64_TA =>
+        -- READ Turnaround
+        when READ_TA =>
           -- o_trdy_l already set to high.
           -- o_devsel_l already set to low.
-          state <= READ64_WAIT;
+          state <= READ64_WAIT when o_ack64_l = '0' else READ32_WAIT;
+        -- READ wait state
         when READ64_WAIT =>
           o_trdy_l <= '0';
+          
           state <= READ64;
         when READ64 =>
+          -- Check IRDY asserted, TRDY, DEVSEL controled by mem card
           if (i_irdy_l = '0') then
+            -- Transmit data, load Data to PCI lines
+            for i in 0 to 3 loop
+              -- Lower 32 bits
+              -- Check if data should be loaded from shadow
+              if (s_shadow_valid = '1') then
+                io_addr_data((8 * i + 7) downto (8 * i)) <= s_data_shadow((8 * i + 7) downto (8 * i)) when i_cbe_lower_l(i) = '0' else (others => '0');
+              -- Load straight from memory
+              else
+                io_addr_data((8 * i + 7) downto (8 * i)) <= s_datas(i) when i_cbe_lower_l(i) = '0' else (others => '0');
+              end if;
+              -- Upper 32 bits
+              -- Byte enabled, load data
+              -- Check if data should be loaded from shadow
+              if (s_shadow_valid = '1') then
+                io_data_upper((8 * i + 7) downto (8 * i)) <= s_data_shadow((8 * (i + 4) + 7) downto (8 * (i + 4))) when i_cbe_upper_l(i) = '0' else (others => '0');
+              -- Load straight from memory
+              else
+                io_data_upper((8 * i + 7) downto (8 * i)) <= s_datas(i + 4) when i_cbe_upper_l(i) = '0' else (others => '0');
+              end if;
+            end loop;
             -- Bust Mode
             if (i_frame_l = '0') then
-
-            -- End Transmission
+              -- Increment address
+              s_addr <= s_addr + 8 - (s_addr mod 8);
+              
+            -- End Transmission w/ data
             else
-              -- Output signals need to be held for the last data xfer.
-              -- Return to tristate on return to idle.
+              -- Set outputs to inactive to complete transmission
+              -- Returns to tristate on return to idle.
+              o_devsel_l => '1';
+              o_trdy_l => '1';
+              o_ack64_l = '1';
               state <= IDLE;
             end if;
+          else
+            if (s_shadow_valid = '0') then
+              s_data_shadow <= s_datas(7) & s_datas(6) & s_datas(5) & s_datas(4) & s_datas(3) & s_datas(2) & s_datas(1) & s_datas(0);
+              s_shadow_valid <= '1';
+            end if;
           end if;
-          
+        when STOP_TERM =>
+          -- Wait for PCI termination protocol
+          if (i_irdy_l = '0' and i_frame_l = '1') then
+            state <= IDLE;
       end case;
     end if;
   end process;
@@ -165,9 +218,9 @@ begin
       i_clk => i_clk,
       i_reset => i_reset,
 
-      i_addr => io_addr_data (17 downto 3),
-      io_data => io_addr_data(7 downto 0),
-      i_cs => i_cbe_lower(0),
+      i_addr => s_addr (17 downto 3),
+      io_data => s_datas(0),
+      i_cs => s_cs(0),
       i_oe => s_readnWrite,
       i_we => not s_readnWrite
     );
@@ -178,9 +231,9 @@ begin
       i_clk => i_clk,
       i_reset => i_reset,
 
-      i_addr => io_addr_data(17 downto 3),
-      io_data => io_addr_data(15 downto 8),
-      i_cs => i_cbe_lower(1),
+      i_addr => s_addr(17 downto 3),
+      io_data => s_datas(1),
+      i_cs => s_cs(1),
       i_oe => s_readnWrite,
       i_we => not s_readnWrite
     );
@@ -191,9 +244,9 @@ begin
       i_clk => i_clk,
       i_reset => i_reset,
 
-      i_addr => io_addr_data(17 downto 3),
-      io_data => io_addr_data(23 downto 16),
-      i_cs => i_cbe_lower(2),
+      i_addr => s_addr(17 downto 3),
+      io_data => s_datas(2),
+      i_cs => s_cs(2),
       i_oe => s_readnWrite,
       i_we => not s_readnWrite
     );
@@ -204,9 +257,60 @@ begin
       i_clk => i_clk,
       i_reset => i_reset,
 
-      i_addr => io_addr_data(17 downto 3),
-      io_data => io_addr_data(31 downto 24),
-      i_cs => i_cbe_lower(3),
+      i_addr => s_addr(17 downto 3),
+      io_data => s_datas(3),
+      i_cs => s_cs(3),
+      i_oe => s_readnWrite,
+      i_we => not s_readnWrite
+    );
+  memory_module_top_inst_4 : entity work.memory_module_top
+    port map
+    (
+      i_clk => i_clk,
+      i_reset => i_reset,
+
+      i_addr => s_addr (17 downto 3),
+      io_data => s_datas(4),
+      i_cs => s_cs(4),
+      i_oe => s_readnWrite,
+      i_we => not s_readnWrite
+    );
+  
+  memory_module_top_inst_5 : entity work.memory_module_top
+    port_map
+    (
+      i_clk => i_clk,
+      i_reset => i_reset,
+
+      i_addr => s_addr(17 downto 3),
+      io_data => s_datas(5),
+      i_cs => s_cs(5),
+      i_oe => s_readnWrite,
+      i_we => not s_readnWrite
+    );
+  
+  memory_module_top_inst_6 : entity work.memory_module_top
+    port_map
+    (
+      i_clk => i_clk,
+      i_reset => i_reset,
+
+      i_addr => s_addr(17 downto 3),
+      io_data => s_datas(6),
+      i_cs => s_cs(6),
+      i_oe => s_readnWrite,
+      i_we => not s_readnWrite
+    );
+
+  memory_module_top_inst_7 : entity work.memory_module_top
+    port_map
+    (
+      i_clk => i_clk,
+      i_reset => i_reset,
+
+      i_addr => s_addr(17 downto 3),
+      io_data => s_datas(7),
+      i_cs => s_cs(7),
       i_oe => s_readnWrite,
       i_we => not s_readnWrite
     );
--- src/memory_module_top.vhd
+++ src/memory_module_top.vhd
@@ -7,19 +7,54 @@ library work;
 use work.global_package.all
 
 entity memory_module_top is
-    port
-    (
-        -- Clock/Reset
-        i_clk       : in std_logic;
-        i_reset     : in std_logic;
+  port
+  (
+    -- Clock/Reset
+    i_clk       : in std_logic;
+    i_reset     : in std_logic;
 
-        -- Memory Module Control Signals
-        i_addr      : in std_logic_vector(14 downto 0);
-        io_data     : inout std_logic_vector(7 downto 0);
-        i_cs        : in std_logic;
-        i_oe        : in std_logic;
-        i_we        : in std_logic
-    );
+    -- Memory Module Control Signals
+    i_addr      : in std_logic_vector(14 downto 0);
+    io_data     : inout std_logic_vector(7 downto 0);
+    i_cs        : in std_logic;
+    i_oe        : in std_logic;
+    i_we        : in std_logic
+  );
 end memory_module_top;
 
+architecture memory_module_top of memory_module_top is
+--------------------- CONSTANTS ---------------------
+  constant c_memory_depth    : integer   := 32768;
 
+---------------------- SIGNALS ----------------------
+  signal s_memory_blk     : t_array_slv (0 to c_memory_depth - 1)(7 downto 0);
+
+begin
+  -- Reset
+  process (i_reset) is
+    if i_reset = '1' then
+      -- Reset all memory values to 0
+      for i in 0 to c_memory_depth - 1 loop
+        s_memory_blk(i) <= (others => '0');
+      end loop;
+      -- TODO: Don't drive output lines, may need to change
+      io_data <= (others => 'Z');
+    end if;
+  end process;
+  
+  process (i_clk) is
+  begin
+    if (rising_edge(i_clk)) then
+      -- Allow memory card to drive io_data by default
+      io_data <= (others => 'Z');
+      -- Memory Read
+      if (i_cs = '1' and i_oe = '1' and i_we = '0') then
+        io_data <= s_memory_blk(i_addr);
+      -- Memory Write
+      elsif (i_cs = '1' and i_oe = '0' and i_we = '1') then 
+        s_memory_blk(i_addr) <= io_data;
+      end if;
+    end if;
+  end process;
+
+end architecture;
